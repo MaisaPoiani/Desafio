d499fc59a415894ddc19d68f4037906aeccc9932
MaisaPoiani <maisampoiani@hotmail.com>
2021-08-18 00:15:19 -0300

    bd

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import { Spinner } from 'reactstrap';
 import Index from '../Components/Index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
 
-function HomePage() {
-    return (
+const Index = (data) => ( // parenteses pois é uma array function, depois não preciso usar o return
         <div>
             <Index />
             Página Inicial
@@ -14,8 +14,7 @@ function HomePage() {
                     <Spinner color="dark" />
             </div>
         </div>
-        );
-}
+)
   
 export class MapContainer extends Component {
     render() {
@@ -24,15 +23,21 @@ export class MapContainer extends Component {
           google={this.props.google}
           zoom={7}
           initialCenter={{ lat: -15.838317, lng: -47.955428 }}
+            // poderia usar {data.response.localizacao} para puxar do BD
         >
         </Map>
       );
     }
 }
-
 export default GoogleApiWrapper(
     (props) => ({
       apiKey: '',
     }
   ))(MapContainer)
-export default HomePage
+
+Index.getInitialPropos = async () => {
+    var response = await axios.get("http://localhost:8080/index");
+    console.log(response);
+    return {response: response.data}
+}
+export default Index
