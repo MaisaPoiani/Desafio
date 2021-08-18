09c7dc2969980b39a55770390cfa531e89c5ddf6
MaisaPoiani <maisampoiani@hotmail.com>
2021-08-17 22:12:49 -0300

    map

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Spinner } from 'reactstrap';
 import Index from '../Components/Index';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from 'react';
 
 function HomePage() {
     return (
@@ -13,6 +15,24 @@ function HomePage() {
             </div>
         </div>
         );
-  }
+}
   
-  export default HomePage
+export class MapContainer extends Component {
+    render() {
+      return (
+        <Map
+          google={this.props.google}
+          zoom={7}
+          initialCenter={{ lat: -15.838317, lng: -47.955428 }}
+        >
+        </Map>
+      );
+    }
+}
+
+export default GoogleApiWrapper(
+    (props) => ({
+      apiKey: '',
+    }
+  ))(MapContainer)
+export default HomePage
